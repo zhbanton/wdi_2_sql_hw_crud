7c2ba4bf448c12ea5d8e2d59241f790505d145fb
Alex Grant <grantovich@users.noreply.github.com>
2014-04-30 14:47:37 -0400

    Make table name and other terminology consistent

--- bank_homework.sql
+++ bank_homework.sql
@@ -8,40 +8,39 @@
 
 
 -- Create a payments table
---  The table should have columns of id, created_at, amount, acct_owner_name,
---  description, and recipient_name with the appropriate datatypes
+--  The table should have columns of id, created_at, amount, payer_name,
+--  recipient_name, and description with the appropriate datatypes
 
 
--- Alter the account_transactions, add the column of `zipcode`
+-- Alter the payments table, add the column of `zipcode`
 -- to specify where the transaction was made
 
 
--- Insert 20 rows into the account_transactions table
--- The first of these transactions should be deposits
--- try to keep the accounts positive and have at least three account owners.
+-- Insert 20 rows into the payments table
+-- The first of these payments should be deposits
+-- try to keep the accounts positive and have at least three different payers.
 
 
--- You receive notice that the transactions with id of 9 is $10 too low.
+-- You receive notice that the payment with id of 9 is $10 too low.
 -- Alter that row so that it is corrected
 
 
--- Delete all transactions with an amount under $2
+-- Delete all payments with an amount under $2
 
 
--- Select all entries from the account_transactions table
+-- Select all entries from the payments table
 
 
--- In a single query, output all transactions ordered from the largest to the smallest
+-- In a single query, output all payments ordered from the largest to the smallest
 
 
--- In a single query, output the names of all distinct recipient_names
+-- In a single query, output the names of all distinct recipients
 
 
--- In a single query, select all transactions created in the past week
+-- In a single query, select all payments created in the past week
 
 
--- In a single query, select all transactions with a blank description
+-- In a single query, select all payments with a blank description
 
 
--- In a single query, select all transactions matching the name of one of the account owners (choose any single one)
-
+-- In a single query, select all payments matching the name of one of the payers (choose any single one)
